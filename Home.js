cce7a24c8139ae2946a390e41856d88236c87991
kiburei <akiburei@gmail.com>
2018-01-18 10:52:18 +0300

    Using Props We use them to customize components. So I've made a class Greeting that says Hi to person X or your Ex. But I dont want to keep typing hey Liz, hey Agnes, hey Joyce bla blais too much for this peng girls. So I make a prop that has the hey part and maybe some roses hearts at then then in the export class ill jus call the greeting component and add the girls names. Kanambrugo

--- Home.js
+++ Home.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 
+class Greeting extends Component {
+  render() {
+    return (
+      <Text>Hello { this.props.name } !</Text>
+    );
+  }
+}
+
 export default class Home extends Component {
   render() {
     return (
-      <View>
-        <Text>This is Home, Greeting from Andrew</Text>
+      <View style={{alignItems: 'center'}}>
+        <Greeting name='Andrew' />
+        <Greeting name='Engineer' />
       </View>
     );
   }
