70d9f6fd2508332b42f84a01618d52f37e262769
kiburei <akiburei@gmail.com>
2018-01-18 11:47:09 +0300

    Blinker...really didnt quite get things here

--- Home.js
+++ Home.js
@@ -1,25 +1,35 @@
 import React, { Component } from 'react';
 import { Text, View, Image } from 'react-native';
 
-class Greeting extends Component {
+class Blink extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {isShowingText: true};
+
+    // Toggle the state every second
+    setInterval(() => {
+      this.setState(previousState => {
+        return { isShowingText: !previousState.isShowingText };
+      });
+    }, 1000);
+  }
+
   render() {
+    let display = this.state.isShowingText ? this.props.text : ' ';
     return (
-      <Text>Hi { this.props.name }, you is peng!</Text>
+      <Text>{display}</Text>
     );
   }
 }
 
-export default class Home extends Component {
+export default class BlinkApp extends Component {
   render() {
-    let potato = {
-      uri: 'http://www.moneysense.ca/wp-content/uploads/2016/12/hot-potato.jpeg'
-    };
     return (
-      <View style={{alignItems: 'center'}}>
-        <Greeting name='Ayra' />
-        <Greeting name='Kamar' />
-        <Greeting name='Carol' />
-        <Image source={potato} style={{width: 300, height: 310}}/>
+      <View>
+        <Blink text='I love to blink' />
+        <Blink text='Yes blinking is so great' />
+        <Blink text='Why did they ever take this out of HTML' />
+        <Blink text='Look at me look at me look at me' />
       </View>
     );
   }
