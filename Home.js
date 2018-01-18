64df41bf9d52483eceb2fe6ce98b9b0412818ea5
kiburei <akiburei@gmail.com>
2018-01-18 11:26:07 +0300

    Add Image Image is component in react native

--- Home.js
+++ Home.js
@@ -1,20 +1,25 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Image } from 'react-native';
 
 class Greeting extends Component {
   render() {
     return (
-      <Text>Hello { this.props.name } !</Text>
+      <Text>Hi { this.props.name }, you is peng!</Text>
     );
   }
 }
 
 export default class Home extends Component {
   render() {
+    let potato = {
+      uri: 'http://www.moneysense.ca/wp-content/uploads/2016/12/hot-potato.jpeg'
+    };
     return (
       <View style={{alignItems: 'center'}}>
-        <Greeting name='Andrew' />
-        <Greeting name='Engineer' />
+        <Greeting name='Ayra' />
+        <Greeting name='Kamar' />
+        <Greeting name='Carol' />
+        <Image source={potato} style={{width: 300, height: 310}}/>
       </View>
     );
   }
